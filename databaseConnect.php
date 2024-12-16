6026d7ade3ec4c066a52857009e5f5e5a34692f8
NikkHubThe1st <nickgimza@hotmail.co.uk>
2024-12-16 13:22:27 +0000

    DBConnection Drama pt.1
    -1 Added a simple DatabaseConnect/php script to connect to DB and return errors
    
    - Renamed all dashboard.html to index.html to work with nuwebspace

--- databaseConnect.php
+++ databaseConnect.php
@@ -1,15 +1,15 @@
 <?php
+try {
     /* This script is used to create a connection to the NewNuMySpace DB using variables to keep things clear*/
     $host = 'localhost';
-    $user = 'unn_w21029182'; /*using my details - Nick */
+    $user = 'w21029182'; /*using my details - Nick */
     $pass = '231ScranSaved'; /*Need to reset password on NewNuMySpace */
-    $dbname = 'unn_w21029182';
-    $connection new PDO("mysql:host=$host:dbname=$dbname:$user,$pass");
-    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    return $connection
-
-    /*Return connection error message on failure */
-    catch(Exception $e)[
-        throw new Exception("Connection error: " . $e->getMessage(),0,$e);
-    ]
+    $dbname = 'w21029182';
+    $connection = new PDO("mysql:host=$host:dbname=$dbname:$user,$pass");
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $connection;
+}
+/*Return connection error message on failure */ catch (Exception $e) {
+    throw new Exception("Connection error: " . $e->getMessage(), 0, $e);
+}
 ?>
